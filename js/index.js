3370f370023da974a476487f0c78506a0204ad95
Randy Miller <randy918@gmail.com>
2024-12-01 14:26:27 -0600

    good 2 label start 12-01-24-1426

--- js/index.js
+++ js/index.js
@@ -1,15 +1,22 @@
+
 document.addEventListener("DOMContentLoaded", function () {
     // Your code here
 
     document.body.style.backgroundColor = 'green';
 
-    //>  ████████████████████████████████████  VARIABLES
+    //>  ████████████████████████████████████  CHILDREN
+// let ulChildren = document.ul.children;
+// let ulChildNodes = document.querySelector('ul').childNodes;
+// console.log(ulChildren[0]);
+// console.log(ulChildNodes[0]);
 
-   
-    //>  ████████████████████████████████████  LISTENERS
 
-  
 
+
+
+
+
+    
     //>  ████████████████████████████████████  BRIGHT BACKGROUND
 
     function getBrightRandomRGB() {
@@ -21,18 +28,46 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     
     document.body.style.backgroundColor = getBrightRandomRGB();
+    
+    //_ ████████████████████████████████████  DATA STRUCTURES 
+    
+    //_ ████████████████████████████████████  DATA STRUCTURES 
+    
+    
+//>  ████████████████████████████████████  GENERAL PROGRAM
 
-//_ ████████████████████████████████████  DATA STRUCTURES 
+let p = document.getElementsByTagName('p')[2];
 
+let previousS = p.previousSibling;
 
+console.log(previousS);
+console.log(previousS);
 
-//_ ████████████████████████████████████  DATA STRUCTURES 
-
+previousS.previousSibling.style.backgroundColor = 'blue';
 
 
 //>  ████████████████████████████████████  GENERAL PROGRAM
 
 
+
+let bodyChildren = document.body.children;
+console.log(bodyChildren.length);
+
+let div = document.getElementsByTagName('div')[0];
+
+let grandparent =  div.parentNode.parentNode;
+console.log(grandparent);
+
+grandparent.style.backgroundColor = 'red';
+
+let h1 = document.getElementsByTagName('h1')[0];
+console.log(h1);
+
+
+
+
+
+// console.log(document.documentElement);
 
     //< ████████████████████████████████████  MISC
 
